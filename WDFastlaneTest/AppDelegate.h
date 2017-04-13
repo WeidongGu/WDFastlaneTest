96584910e802f92950fcaac2aa76578c4e45e771
Weidong Gu <wdgu@thoughtworks.com>
2017-04-13 19:57:47 +0800

    first commit

---
+++ WDFastlaneTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WDFastlaneTest
+//
+//  Created by Weidong Gu  on 13/04/2017.
+//  Copyright © 2017 tw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WDFastlaneTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WDFastlaneTest
+//
+//  Created by Weidong Gu  on 13/04/2017.
+//  Copyright © 2017 tw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
